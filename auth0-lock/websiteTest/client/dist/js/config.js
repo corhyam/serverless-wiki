b57278a081a1db2a768bd29b76da2981b9d87317
Corhyam <840420393@qq.com>
2021-03-11 23:22:27 +0800

    create client/dist and create replica in it

---
+++ auth0-lock/websiteTest/client/dist/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+	auth0: {
+		domain: 'corhyam.jp.auth0.com',
+		clientId: 'VmcELwLZiva0YDmspxAgDwzCTieSydAN'
+	},
+  apiBaseUrl: 'https://API-GATEWAY-URL/dev'
+};
---
+++ auth0-lock/websiteTest/client/dist/js/main.js
@@ -0,0 +1,5 @@
+(function(){
+	$(document).ready(function(){
+		userController.init(configConstants);//运行userController.init函数关联事件，设置auth0
+	});
+}());
