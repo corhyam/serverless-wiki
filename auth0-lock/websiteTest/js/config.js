53b863abaa39bbe1e44bef145b18db89d1597aca
Corhyam <840420393@qq.com>
2021-03-12 17:23:48 +0800

    delete replica website and pic

--- auth0-lock/websiteTest/js/config.js
+++
@@ -1,7 +0,0 @@
-var configConstants = {
-	auth0: {
-		domain: 'corhyam.jp.auth0.com',
-		clientId: 'VmcELwLZiva0YDmspxAgDwzCTieSydAN'
-	},
-  apiBaseUrl: 'https://API-GATEWAY-URL/dev'
-};
--- auth0-lock/websiteTest/js/main.js
+++
@@ -1,5 +0,0 @@
-(function(){
-	$(document).ready(function(){
-		userController.init(configConstants);//运行userController.init函数关联事件，设置auth0
-	});
-}());
