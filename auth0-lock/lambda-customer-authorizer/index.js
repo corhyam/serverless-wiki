e468d7699aef71cb5d286c6e51ec1f5997c0b440
Corhyam <840420393@qq.com>
2021-03-13 17:47:56 +0800

    add lambda fun customer-authorizer

---
+++ auth0-lock/lambda-customer-authorizer/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var jwt = require('jsonwebtoken');
+//generatePolicy方法用于生成一个IAM的策略，根据生成的是 'Allow' 或者 'Deny' 来判断，该用户是否拥有权限。
+// 策略规定api gateway可调用的资源
+var generatePolicy = function(principalId, effect, resource) {
+    var authResponse = {};
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        var policyDocument = {};
+        policyDocument.Version = '2012-10-17';
+        policyDocument.Statement = [];
+        var statementOne = {};
+        statementOne.Action = 'execute-api:Invoke';
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
+    }
+    return authResponse;
+}
+
+exports.handler = function(event, context, callback){
+    if (!event.authorizationToken) {
+    	callback('Could not find authToken');
+    	return;
+    }
+
+    var token = event.authorizationToken.split(' ')[1];
+
+    var secretBuffer = new Buffer(process.env.AUTH0_SECRET);//可以通过环境变量获取 在lambda里面配了
+    jwt.verify(token, secretBuffer, function(err, decoded){//根据 AUTH0 的 ID 和密钥来校验 token 是否是有效
+    	if(err){
+    		console.log('Failed jwt verification: ', err, 'auth: ', event.authorizationToken);
+    		callback('Authorization Failed');
+    	} else {//判断令牌是否有效 有效就返回个策略
+    		callback(null, generatePolicy('user', 'allow', event.methodArn));
+    	}
+    })
+};
