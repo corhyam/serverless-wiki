25003820fe2fc8d07cacec8271b5b092f96b56b3
Corhyam <840420393@qq.com>
2021-03-17 22:54:17 +0800

    auth0 user-profile test

--- auth0-lock/lambda-customer-authorizer/index.js
+++ auth0-lock/lambda-customer-authorizer/index.js
@@ -22,19 +22,19 @@ var generatePolicy = function(principalId, effect, resource) {
 
 exports.handler = function(event, context, callback){
     if (!event.authorizationToken) {
-    	callback('Could not find authToken');
-    	return;
+        callback('Could not find authToken');
+        return;
     }
 
     var token = event.authorizationToken.split(' ')[1];
     //const AUTH0_CLIENT_PUBLIC_KEY = new Buffer(process.env.AUTH0_CLIENT_PUBLIC_KEY);//这里测试一下用public key能不能解码
-    var secretBuffer = new Buffer(process.env.AUTH0_SECRET,);//可以通过环境变量获取 在lambda里面配了
-    jwt.verify(token, secretBuffer, {algorithms: ['RS256']},function(err, decoded){//根据 AUTH0 的 ID 和密钥来校验 token 是否是有效
-    	if(err){
-    		console.log('Failed jwt verification: ', err, 'auth: ', event.authorizationToken);
-    		callback('Authorization Failed');
-    	} else {//判断令牌是否有效 有效就返回个策略
-    		callback(null, generatePolicy('user', 'allow', event.methodArn));
-    	}
+    var secretBuffer = new Buffer(process.env.AUTH0_SECRET);//可以通过环境变量获取 在lambda里面配了
+    jwt.verify(token, secretBuffer, function(err, decoded){//根据 AUTH0 的 ID 和密钥来校验 token 是否是有效
+        if(err){
+            console.log('Failed jwt verification: ', err, 'auth: ', event.authorizationToken);
+            callback('Authorization Failed');
+        } else {//判断令牌是否有效 有git效就返回个策略
+            callback(null, generatePolicy('user', 'allow', event.methodArn));
+        }
     })
 };
--- auth0-lock/lambda-user-profile/index.js
+++ auth0-lock/lambda-user-profile/index.js
@@ -1,43 +1,44 @@
 //这个js主也要负责令牌的校验和解码，成功则向auth0提供的tokeninfo endpoint发送请求。
 // jwt也包含在请求中，tokeninfo返回用户信息，发回网站。大致思路
+
 'use strict';
 
 var jwt = require('jsonwebtoken');
 var request = require('request');
 
 exports.handler = function(event, context, callback){
-    if (!event.authToken) {        //由网站通过authorization标头传入的jwt令牌
-    	callback('Could not find authToken');
-    	return;
+    if (!event.authToken) {
+        callback('Could not find authToken');
+        return;
     }
 
     var token = event.authToken.split(' ')[1];
 
     var secretBuffer = new Buffer(process.env.AUTH0_SECRET);
     jwt.verify(token, secretBuffer, function(err, decoded){
-    	if(err){
-    		console.log('Failed jwt verification: ', err, 'auth: ', event.authToken);
-    		callback('Authorization Failed');
-    	} else {
+        if(err){
+            console.log('Failed jwt verification: ', err, 'auth: ', event.authToken);
+            callback('Authorization Failed');
+        } else {
 
-        var body = {
-          'id_token': token
-        };
+            var body = {
+                'id_token': token
+            };
 
-        var options = {
-          url: 'https://'+ process.env.DOMAIN + '/tokeninfo',
-          method: 'POST',
-          json: true,
-          body: body
-        };
+            var options = {
+                url: 'https://'+ process.env.DOMAIN + '/userinfo',
+                method: 'GET',
+                json: true,
+                body: body
+            };
 
-        request(options, function(error, response, body){
-          if (!error && response.statusCode === 200) {
-            callback(null, body);
-          } else {
-            callback(error);
-          }
-        });
-    	}
+            request(options, function(error, response, body){
+                if (!error && response.statusCode === 200) {
+                    callback(null, body);
+                } else {
+                    callback(error);
+                }
+            });
+        }
     })
 };
