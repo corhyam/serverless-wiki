55b9ecf1b0794bc62267f527a9901896dd920739
Corhyam <840420393@qq.com>
2021-03-25 00:00:12 +0800

    update index.js

--- auth0-lock/lambda-user-profile/index.js
+++ auth0-lock/lambda-user-profile/index.js
@@ -1,6 +1,5 @@
 //这个js主也要负责令牌的校验和解码，成功则向auth0提供的tokeninfo endpoint发送请求。
 // jwt也包含在请求中，tokeninfo返回用户信息，发回网站。大致思路
-
 'use strict';
 
 var jwt = require('jsonwebtoken');
@@ -12,17 +11,19 @@ exports.handler = function(event, context, callback){
         return;
     }
 
-    var token = event.authToken.split(' ')[1];
+    var id_token = event.authToken.split(' ')[1];//取id Token的值 拆分后第二个
+    var access_token = event.accessToken;
 
     var secretBuffer = new Buffer(process.env.AUTH0_SECRET);
-    jwt.verify(token, secretBuffer, function(err, decoded){
+    jwt.verify(id_token, secretBuffer, function(err, decoded){
         if(err){
             console.log('Failed jwt verification: ', err, 'auth: ', event.authToken);
-            callback('Authorization Failed');
+            callback('Authorization Failed: ' + id_token + ", error: " + err + ", auth: " + event.authToken);
         } else {
 
             var body = {
-                'id_token': token
+                'id_token': id_token,
+                'access_token': access_token
             };
 
             var options = {
@@ -33,7 +34,9 @@ exports.handler = function(event, context, callback){
             };
 
             request(options, function(error, response, body){
+                console.log("Response0: " + JSON.stringify(response));
                 if (!error && response.statusCode === 200) {
+                    console.log("Response1: " + JSON.stringify(response));
                     callback(null, body);
                 } else {
                     callback(error);
@@ -42,3 +45,4 @@ exports.handler = function(event, context, callback){
         }
     })
 };
+
